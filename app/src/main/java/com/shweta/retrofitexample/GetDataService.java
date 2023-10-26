f6ad384545712474fc5e405f3eba8aa366a4eb45
Shweta <shweta.gadhave09@gmail.com>
2023-10-26 11:42:09 +0530

    Initial commit

---
+++ app/src/main/java/com/shweta/retrofitexample/GetDataService.java
@@ -0,0 +1,15 @@
+package com.shweta.retrofitexample;
+
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("/photos")
+    Call<List<RetroPhoto>> getAllPhotos();
+
+}
